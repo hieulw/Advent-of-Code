856ef805b6f44bd17979de7c852da48285683414
Hieu Le <hieu.le@fiisoft.com>
2023-12-03 21:53:20 +0700

    second day part two

--- 2023/golang/day2/main.go
+++ 2023/golang/day2/main.go
@@ -10,11 +10,18 @@ import (
 
 func main() {
 	sum := 0
+	sumPower := 0
+	power := 1
 	possible := true
 	threshold := map[string]int{
 		"red":   12,
 		"green": 13,
 		"blue":  14,
+	}
+	max := map[string]int{
+		"red":   0,
+		"green": 0,
+		"blue":  0,
 	}
 	file, err := os.Open("input")
 	if err != nil {
@@ -40,12 +47,26 @@ func main() {
 				if number > threshold[color] {
 					possible = false
 				}
+				if max[color] < number {
+					max[color] = number
+				}
 			}
 		}
 		if possible {
 			sum += gameId
 		}
 		possible = true
+		for _, number := range max {
+			power *= number
+		}
+		sumPower += power
+		power = 1
+		max = map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
 	}
-	fmt.Println(sum)
+	fmt.Println("part 1:", sum)
+	fmt.Println("part 2:", sumPower)
 }
