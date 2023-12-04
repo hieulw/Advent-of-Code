ef25ad795186be81e9566cbeb3cb168143505521
Hieu Le <hieu.le@fiisoft.com>
2023-12-04 19:49:57 +0700

    third day part one

---
+++ 2023/golang/day3/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var schematic []string
+	file, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		schematic = append(schematic, line)
+	}
+
+	sum := 0
+	for i, line := range schematic {
+		numStr := ""
+		partNumber := false
+		for j := range line {
+			if isDigit(line[j]) {
+				if isAdjacentToSymbol(schematic, i, j) {
+					partNumber = true
+				}
+				numStr += string(line[j])
+			}
+			// number separator by '.', symbol and end of line
+			if line[j] == '.' || isSymbol(line[j]) || j == len(line)-1 {
+				num, err := strconv.Atoi(numStr)
+				if err == nil && partNumber {
+					sum += num
+				}
+				numStr = ""
+				partNumber = false
+			}
+		}
+	}
+	fmt.Println(sum)
+}
+
+func isDigit(number byte) bool {
+	return number >= '0' && number <= '9'
+}
+
+func isSymbol(char byte) bool {
+	return !isDigit(char) && char != '.'
+}
+
+func isAdjacentToSymbol(schematic []string, line, row int) bool {
+	for i := line - 1; i <= line+1; i++ {
+		if i < 0 || i >= len(schematic) {
+			continue
+		}
+		for j := row - 1; j <= row+1; j++ {
+			if j < 0 || j >= len(schematic[i]) {
+				continue
+			}
+			if isSymbol(byte(schematic[i][j])) {
+				return true
+			}
+		}
+	}
+	return false
+}
