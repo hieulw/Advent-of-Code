57fbd311f0ffded094db9501f66cfdc94e384398
Hieu Le <hieu.le@fiisoft.com>
2023-12-05 11:42:14 +0700

    fourth day part two

--- 2023/golang/day4/main.go
+++ 2023/golang/day4/main.go
@@ -6,17 +6,22 @@ import (
 	"math"
 	"os"
 	"slices"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
+	scratchcards := make(map[int]int)
+	totalCard := 0
 	sum := 0
 	count := 0
+
+	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
 		s := strings.Split(line, ": ")
+		c := strings.Split(s[0], " ")
+		cardNumber, _ := strconv.Atoi(c[len(c)-1])
 		s = strings.Split(s[1], " | ")
 		winNumbers := deleteEmptyElement(strings.Split(s[0], " "))
 		numbers := deleteEmptyElement(strings.Split(s[1], " "))
@@ -25,13 +30,21 @@ func main() {
 				count++
 			}
 		}
+		scratchcards[cardNumber]++
 		if count != 0 {
 			points := int(math.Pow(2, float64(count-1)))
 			sum += points
+			for i := cardNumber + 1; i < cardNumber+count+1; i++ {
+				scratchcards[i] += scratchcards[cardNumber]
+			}
 		}
 		count = 0
 	}
+	for _, c := range scratchcards {
+		totalCard += c
+	}
 	fmt.Println(sum)
+	fmt.Println(totalCard)
 }
 
 func deleteEmptyElement(s []string) []string {
