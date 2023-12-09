0958d155e7e7a9996e58a8e8d8eff6a410071afd
Hieu Le <hieu.le@fiisoft.com>
2023-12-09 14:35:20 +0700

    eighth day part two

--- 2023/day8/main.go
+++ 2023/day8/main.go
@@ -11,8 +11,8 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 
+	var start []string
 	instruction := scanner.Text()
-	start, end := "AAA", "ZZZ"
 	maps := map[string][2]string{}
 
 	for scanner.Scan() {
@@ -26,12 +26,28 @@ func main() {
 		left := strings.TrimLeft(split[0], "(")
 		right := strings.TrimRight(split[1], ")")
 		maps[current] = [2]string{left, right}
+		if strings.HasSuffix(current, "A") {
+			start = append(start, current)
+		}
 	}
 
-	count := 0
+	steps := []int{}
+	for i := range start {
+		steps = append(steps, countSteps(maps, instruction, start[i]))
+	}
+	// finding least common multiple of all steps
+	lcmSteps := steps[0]
+	for i := range steps {
+		lcmSteps = lcm(lcmSteps, steps[i])
+	}
+	fmt.Println(lcmSteps)
+}
+
+func countSteps(maps map[string][2]string, instruction, start string) int {
 	current := start
+	count := 0
 	i := 0
-	for current != end {
+	for !strings.HasSuffix(current, "Z") {
 		switch instruction[i] {
 		case 'L':
 			current = maps[current][0]
@@ -41,5 +57,16 @@ func main() {
 		count++
 		i = count % len(instruction)
 	}
-	fmt.Println(count)
+	return count
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
