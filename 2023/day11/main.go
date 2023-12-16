ca78ae2608d2f35d75ecd1a455b20be29841097d
Hieu Le <hieu.le@fiisoft.com>
2023-12-16 19:50:29 +0700

    11th day part two

--- 2023/day11/main.go
+++ 2023/day11/main.go
@@ -20,15 +20,36 @@ func main() {
 		universe = append(universe, []rune(scanner.Text()))
 	}
 
-	universe = expandUniverse(universe)
+	x, y := expandUniverse(universe)
 	galaxies := findGalaxy(universe)
+	galaxies = galaxyAfterExpand(galaxies, x, y, 1e6)
 	sum := 0.0
 	for i := range galaxies {
 		for j := i + 1; j < len(galaxies); j++ {
-			sum += math.Abs(float64(galaxies[i].x-galaxies[j].x)) + math.Abs(float64(galaxies[i].y-galaxies[j].y))
+			sum += shortestPath(galaxies[i], galaxies[j])
 		}
 	}
 	fmt.Printf("%.0f\n", sum)
+}
+
+func galaxyAfterExpand(galaxies []Galaxy, x, y []int, expandTime int) []Galaxy {
+	expandTime -= 1
+	for i := range galaxies {
+		expandX, expandY := 0, 0
+		for j := 0; j < len(x) && galaxies[i].x > x[j]; j++ {
+			expandX += expandTime
+		}
+		for j := 0; j < len(y) && galaxies[i].y > y[j]; j++ {
+			expandY += expandTime
+		}
+		galaxies[i].x += expandX
+		galaxies[i].y += expandY
+	}
+	return galaxies
+}
+
+func shortestPath(a, b Galaxy) float64 {
+	return math.Abs(float64(a.x-b.x)) + math.Abs(float64(a.y-b.y))
 }
 
 func findGalaxy(universe [][]rune) []Galaxy {
@@ -44,14 +65,13 @@ func findGalaxy(universe [][]rune) []Galaxy {
 	return galaxies
 }
 
-func expandUniverse(universe [][]rune) [][]rune {
-	for i := 0; i < len(universe); i++ {
+func expandUniverse(universe [][]rune) (xPoints, yPoints []int) {
+	for i := 0; i < len(universe); i++ { // expand horizontally
 		if !slices.Contains(universe[i], '#') {
-			universe = slices.Insert(universe, i, universe[i])
-			i++
+			yPoints = append(yPoints, i)
 		}
 	}
-	for j := 0; j < len(universe[0]); j++ {
+	for j := 0; j < len(universe[0]); j++ { // expand vertically
 		foundGalaxy := false
 		for i := range universe {
 			if universe[i][j] == '#' {
@@ -62,10 +82,7 @@ func expandUniverse(universe [][]rune) [][]rune {
 		if foundGalaxy {
 			continue
 		}
-		for i := range universe {
-			universe[i] = slices.Insert(universe[i], j, '.')
-		}
-		j++
+		xPoints = append(xPoints, j)
 	}
-	return universe
+	return
 }
